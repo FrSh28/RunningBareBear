2262950d6691cc32c8058a92d95001c9477bd9dc
FrSh28 <37129124+FrSh28@users.noreply.github.com>
2020-12-16 16:38:40 +0800

    Testing "includeSDL.h"

---
+++ src/includeSDL.h
@@ -0,0 +1,15 @@
+#ifdef _WIN32
+	#include <SDL2/SDL.h>
+	#include <SDL2/SDL_image.h>
+	#include <SDL2/SDL_mixer.h>
+	#include <SDL2/SDL_ttf.h>
+	#include <SDL2/SDL_bgi.h>
+#endif
+
+#ifdef __APPLE__
+	#include <SDL2/SDL.h>
+	#include <SDL2_image/SDL_image.h>
+	#include <SDL2_mixer/SDL_mixer.h>
+	#include <SDL2_ttf/SDL_ttf.h>
+	#include <SDL2/SDL_bgi.h>
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_bgi.h>
+#include "includeSDL.h"
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	initwindow(800, 600);
-	cleardevice();
-
-	circle(100,100,100);
-	cout << "TEST" << endl;
-
-	getch();
-	closegraph();
+	int sdl2 = SDL_INIT_VIDEO;
+	int sdl2_image = IMG_INIT_JPG;
+	int sdl2_mixer = MIX_DEFAULT_FORMAT;
+	int sdl2_ttf = TTF_STYLE_BOLD;
+	int sdl2_bgi = CENTER_TEXT;
+	cout << "TEST";
 	return 0;
 }
