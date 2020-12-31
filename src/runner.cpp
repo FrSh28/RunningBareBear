e8d619a7dcd3af96e5b7815d7544556e0a1bc38a
lsy1205 <75497265+lsy1205@users.noreply.github.com>
2020-12-31 23:06:35 +0800

    add sth in runner

--- src/runner.cpp
+++ src/runner.cpp
@@ -7,7 +7,15 @@ runner::runner(char * ,int )
 {
     if (mode==1)
     {
-
+        //texture = loadMedia(TEXTURE_XXX);
+        /*posOnWindow.x = (int);
+        posOnTexture.x = (int);
+        posOnWindow.y = (int);
+        posOnTexture.y = (int);
+        posOnWindow.w = (int);
+        posOnTexture.w = (int);
+        posOnWindow.h = (int);
+        posOnTexture.h = (int);*/
     }
 }
 
