12b37c1a880fdb88ae91c34da869d41e4decd25b
lsy1205 <75497265+lsy1205@users.noreply.github.com>
2021-01-08 01:36:10 +0800

    finish collision box/move/the clip to be rendered

--- src/Runner.cpp
+++ src/Runner.cpp
@@ -4,12 +4,17 @@
 #include "Runner.h"
 character_list character;
 
-const int WALKING_ANIMATION_FRAMES = 12;
-SDL_Rect Current_Clip [WALKING_ANIMATION_FRAMES];
+const int ANIMATION_FRAMES = 3;
+const int TOTAL_FRAMES = 12;
+SDL_Rect Clip [TOTAL_FRAMES];
 
-Runner::Runner(character_list character ):
-BasicObject("Runner"),Rstrength(100),map(&Map::getMap())
+const int Runner::gridWidth = Map::getPixelWidth();
+const int Runner::gridHeight = Map::getPixelHeight();
+Runner::Runner(SDL_Point& InitialMapPos,SDL_Point& InitialPixelPos, character_list character):
+BasicObject("Runner"), strength(100), map(&Map::getMap()), game(Game::GetGame()), width(40),
+height(32), updateRate(6), direction(Down), CurrentClip(&Clip[0])
 {
+
     initclips();
     if (mode==1)
     {
@@ -26,59 +31,59 @@ BasicObject("Runner"),Rstrength(100),map(&Map::getMap())
         switch(character)
         {
             case professor_hsieh:
-                Rvelocity = 00000;
+                velocity = 00000;
                 sprint_velocity = 00000;
                 username="professor";
                 break;
 
             case bear:
-                Rvelocity = 00000;
+                velocity = 00000;
                 sprint_velocity = 00000;
                 username = "bear";
                 break;
 
             case soldier:
-                Rvelocity = 00000;
+                velocity = 00000;
                 sprint_velocity = 00000;
                 username = "soldier";
                 break;
         }
     }
-    // set big grid on map
-    setMapPos(MapPos);
-    // set small grid on map (center)
-    setPixelPos(PixelPos);
-    // change PixelPos to top left
-    PixelPos.x -= runner width/2 ;
-    PixelPos.y -= runner height/2;
-    // set rect for collision
-    rec_on_map.x = pos_on_map.x;
-    rec_on_map.y = pos_on_map.y;
-    rec_on_map.w = 40;
-    rec_on_map.h = 32;
+    //set Initial PixelPos
+    setPixelPos(InitialPixelPos);
+    //set Initial MapPos
+    setMapPos(InitialMapPos);
 }
 
+void Runner::setMapPos(SDL_Point &Set)
+{
+    MapPos.x = Set.x;
+    MapPos.y = Set.y;
+}
+void Runner::setPixelPos(SDL_Point &Set)
+{
+    PixelPos.x = Set.x;
+    PixelPos.y = Set.y;
+    // change PixelPos to top left
+    PixelPos.x -=  width/2 ;
+    PixelPos.y -=  height/2;
+}
 Runner::~Runner(){/*show win or lose*/}
 SDL_Point Runner::getMapPos()const{return MapPos;}       // get big grid on map
 SDL_Point Runner::getPixelPos()const                     // get small grid on map (center)
 {
     SDL_Point center;
-    center.x = PixelPos.x + character width/2;
-    center.y = PixelPos.y + character height/2;
+    center.x = PixelPos.x + width/2;
+    center.y = PixelPos.y + height/2;
     return center;
 }
 
-void Runner::updategrid(SDL_Point& currentpoint)
-{
-    SDL_Point tmp;
-    tmp.x = currentpoint.x + rec_on_map.w/2;
-    tmp.y = currentpoint.y + rec_on_map.h/2;
-    grid = Map::pixelposition
-}
+void Runner::updateMapPos(SDL_Point& currentMappos)       // ask map for big grid and update MapPos
+{currentMappos = map->getRunnerMapPos();}
 
 bool Runner::handleEvents(SDL_Event &e)
 {
-    bool success=true;
+    bool success=false;
     if(mode==1)
     {
         //if a key is pressed
@@ -87,12 +92,29 @@ bool Runner::handleEvents(SDL_Event &e)
             //adjust velocity
             switch(e.key.keysym.sym)
             {
-                case SDLK_SPACE: Rvelocity += sprint_velocity;
-                case SDLK_s: Rvelocity_y += Rvelocity; break;
-                case SDLK_w: Rvelocity -= Rvelocity; break;
-                case SDLK_d: Rvelocity_x += Rvelocity; break;
-                case SDLK_a: Rvelocity_x -= Rvelocity; break;
-                case SDLK_e: pickup = true;
+                case SDLK_SPACE:
+                    velocity += sprint_velocity;
+                    updateRate = 3;
+                    return true;
+                case SDLK_s:
+                    velocity_y += velocity;
+                    direction = Down;
+                    return  true;
+                case SDLK_w:
+                    velocity -= velocity;
+                    direction = Up;
+                    return true;
+                case SDLK_d:
+                    velocity_x += velocity;
+                    direction = Right;
+                    return true;
+                case SDLK_a:
+                    velocity_x -= velocity;
+                    direction = Left;
+                    return true;
+                case SDLK_e:
+                    pickup = true;
+                    return true;
             }
         }
         //if a key is released
@@ -101,15 +123,26 @@ bool Runner::handleEvents(SDL_Event &e)
             //Adjust the velocity
             switch( e.key.keysym.sym )
             {
-                case SDLK_SPACE: Rvelocity -= sprint_velocity;
-                case SDLK_UP: Rvelocity_y -= Rvelocity; break;
-                case SDLK_DOWN: Rvelocity += Rvelocity; break;
-                case SDLK_LEFT: Rvelocity_x -= Rvelocity; break;
-                case SDLK_RIGHT: Rvelocity_x += Rvelocity; break;
-                case SDLK_e: pickup = false;
+                case SDLK_SPACE:
+                    velocity -= sprint_velocity;
+                    return true;
+                case SDLK_UP:
+                    velocity_y -= velocity;
+                    return true;
+                case SDLK_DOWN:
+                    velocity += velocity;
+                    return true;
+                case SDLK_LEFT:
+                    velocity_x -= velocity;
+                    return true;
+                case SDLK_RIGHT:
+                    velocity_x += velocity;
+                    return true;
+                case SDLK_e:
+                    pickup = false;
+                    return true;
             }
         }
-
     }
     return success;
 }
@@ -117,159 +150,278 @@ bool Runner::handleEvents(SDL_Event &e)
 void Runner::move()
 {
     //right and left
-    pos_on_map.x += Rvelocity_x;
+    PixelPos.x += velocity_x;
     //up and down
-    pos_on_map.y += Rvelocity_y;
-
-    if(collision(getwall()))
-    {
-        //right and left
-        pos_on_map.x -= Rvelocity_x;
-        //up and down
-        pos_on_map.y -= Rvelocity_y;
-    }
+    PixelPos.y += velocity_y;
+    checkCollision();
 }
 
 void Runner::update()
 {
     static int frame = 0;
-
+    // move(update PixelPos)
     move();
-    if(Rvelocity_x!=0||Rvelocity_y!=0)
+    // deal strength
+    if(velocity_x!=0||velocity_y!=0)
     {
-        Rstrength -= 0.05;
-        if(Rvelocity_x == abs(sprint_velocity+Rvelocity)||Rvelocity_y == abs(sprint_velocity+Rvelocity))
-        {Rstrength -= 0.05;}
+        strength -= 0.05;
+        if(abs(velocity_x) == sprint_velocity+velocity || abs(velocity_y) == sprint_velocity+velocity)
+        {strength -= 0.05;}
     }
-    //posOnWindow = Map::convertPos(pos);
-    rec_on_map.x = pos_on_map.x;
-    rec_on_map.y = pos_on_map.y;
+
+    // use map function to calculate MapPos and update
+    MapPos = map->mapPosTopixelPos(SDL_Point({PixelPos.x+width/2,PixelPos.y+height/2}));
+
+    //Render current frame
+    CurrentClip = &Clip[direction*3+frame/updateRate];
+    //render??;
+
+    // go to next frame
     frame ++;
-    updategrid(pos_on_map);
-
-
-    if(frame>1200){frame=0;}
-
+    // cycle animation
+    if(frame/updateRate > ANIMATION_FRAMES){frame=0;}
 }
 
-bool Runner::collision(Wall* wall)
+bool Runner::collisionBox(SDL_Rect& square)
 {
     bool collision = true;
-    if(wall !=NULL)
-    {
-        if (rec_on_map.x > wall.rec_on_map.x+wall.rec_on_map.w){return false;}
-        if(rec_on_map.x+rec_on_map.w < wall.rec_on_map.x){return false;}
-        if(rec_on_map.y < wall.rec_on_map.y+wall.rec_on_map.h){return false;}
-        if(rec_on_map.y+rec_on_map.h > wall.rec_on_map.y){return false;}
-    }
+    if(PixelPos.x        > square.x+square.w){return false;}
+    if(PixelPos.x+width  < square.x){return false;}
+    if(PixelPos.y        < square.y+square.h){return false;}
+    if(PixelPos.y+height > square.y){return false;}
     return collision;
-
 }
 
-Wall* Runner::getwall()
+void Runner::checkCollision()
 {
-    Wall* wall=NULL;
-    //check right grid
-    if(Rvelocity_x>0)
+    // right
+    if(velocity_x>0)
     {
-        if(pos_on_map.x+1 is wall)
+        // check right top
+        if(map->isWall(SDL_Point({MapPos.x+1,MapPos.y-1})))
         {
-            //wall = the wall right;
-            return wall;
+            SDL_Point wallPos;
+            wallPos.x = map->pixelPosTomapPos(SDL_Point({MapPos.x+1,MapPos.y-1})).x;
+            wallPos.y = map->pixelPosTomapPos(SDL_Point({MapPos.x+1,MapPos.y-1})).y;
+            SDL_Rect wall({wallPos.x,wallPos.y,gridWidth,gridHeight});
+            if(collisionBox(wall))
+            {
+                PixelPos.x -= velocity_x;
+            }
+        }
+        // chek right middle
+        if(map->isWall(SDL_Point({MapPos.x+1,MapPos.y})))
+        {
+            SDL_Point wallPos;
+            wallPos.x = map->pixelPosTomapPos(SDL_Point({MapPos.x+1,MapPos.y})).x;
+            wallPos.y = map->pixelPosTomapPos(SDL_Point({MapPos.x+1,MapPos.y})).y;
+            SDL_Rect wall({wallPos.x,wallPos.y,gridWidth,gridHeight});
+            if(collisionBox(wall))
+            {
+                PixelPos.x -= velocity_x;
+            }
+        }
+        // check right bottom
+        if(map->isWall(SDL_Point({MapPos.x+1,MapPos.y+1})))
+        {
+            SDL_Point wallPos;
+            wallPos.x = map->pixelPosTomapPos(SDL_Point({MapPos.x+1,MapPos.y+1})).x;
+            wallPos.y = map->pixelPosTomapPos(SDL_Point({MapPos.x+1,MapPos.y+1})).y;
+            SDL_Rect wall({wallPos.x,wallPos.y,gridWidth,gridHeight});
+            if(collisionBox(wall))
+            {
+                PixelPos.x -= velocity_x;
+            }
         }
     }
 
-    //check left grid
-    if(Rvelocity_x<0)
+    // left
+    if(velocity_x<0)
     {
-        if(pos_on_map.x-1 is wall)
+        // check left top
+        if(map->isWall(SDL_Point({MapPos.x-1,MapPos.y-1})))
         {
-            //wall = the wall left;
-            return wall;
+            SDL_Point wallPos;
+            wallPos.x = map->pixelPosTomapPos(SDL_Point({MapPos.x-1,MapPos.y-1})).x;
+            wallPos.y = map->pixelPosTomapPos(SDL_Point({MapPos.x-1,MapPos.y-1})).y;
+            SDL_Rect wall({wallPos.x,wallPos.y,gridWidth,gridHeight});
+            if(collisionBox(wall))
+            {
+                PixelPos.x -= velocity_x;
+            }
+        }
+        // chek left middle
+        if(map->isWall(SDL_Point({MapPos.x+1,MapPos.y})))
+        {
+            SDL_Point wallPos;
+            wallPos.x = map->pixelPosTomapPos(SDL_Point({MapPos.x-1,MapPos.y})).x;
+            wallPos.y = map->pixelPosTomapPos(SDL_Point({MapPos.x-1,MapPos.y})).y;
+            SDL_Rect wall({wallPos.x,wallPos.y,gridWidth,gridHeight});
+            if(collisionBox(wall))
+            {
+                PixelPos.x -= velocity_x;
+            }
+        }
+        // check right bottom
+        if(map->isWall(SDL_Point({MapPos.x+1,MapPos.y+1})))
+        {
+            SDL_Point wallPos;
+            wallPos.x = map->pixelPosTomapPos(SDL_Point({MapPos.x-1,MapPos.y+1})).x;
+            wallPos.y = map->pixelPosTomapPos(SDL_Point({MapPos.x-1,MapPos.y+1})).y;
+            SDL_Rect wall({wallPos.x,wallPos.y,gridWidth,gridHeight});
+            if(collisionBox(wall))
+            {
+                PixelPos.x -= velocity_x;
+            }
         }
     }
 
-    //check up grid
-    if(Rvelocity_y<0)
+    // up
+    if(velocity_y<0)
     {
-        if(pos_on_map.y-1 is wall)
+        // check top left
+        if(map->isWall(SDL_Point({MapPos.x-1,MapPos.y-1})))
         {
-            //wall = the wall above;
-            return wall;
+            SDL_Point wallPos;
+            wallPos.x = map->pixelPosTomapPos(SDL_Point({MapPos.x-1,MapPos.y-1})).x;
+            wallPos.y = map->pixelPosTomapPos(SDL_Point({MapPos.x-1,MapPos.y-1})).y;
+            SDL_Rect wall({wallPos.x,wallPos.y,gridWidth,gridHeight});
+            if(collisionBox(wall))
+            {
+                PixelPos.y -= velocity_y;
+            }
+        }
+        // chek top middle
+        if(map->isWall(SDL_Point({MapPos.x,MapPos.y-1})))
+        {
+            SDL_Point wallPos;
+            wallPos.x = map->pixelPosTomapPos(SDL_Point({MapPos.x,MapPos.y-1})).x;
+            wallPos.y = map->pixelPosTomapPos(SDL_Point({MapPos.x,MapPos.y-1})).y;
+            SDL_Rect wall({wallPos.x,wallPos.y,gridWidth,gridHeight});
+            if(collisionBox(wall))
+            {
+                PixelPos.y -= velocity_y;
+            }
+        }
+        // check top right
+        if(map->isWall(SDL_Point({MapPos.x+1,MapPos.y-1})))
+        {
+            SDL_Point wallPos;
+            wallPos.x = map->pixelPosTomapPos(SDL_Point({MapPos.x+1,MapPos.y-1})).x;
+            wallPos.y = map->pixelPosTomapPos(SDL_Point({MapPos.x+1,MapPos.y-1})).y;
+            SDL_Rect wall({wallPos.x,wallPos.y,gridWidth,gridHeight});
+            if(collisionBox(wall))
+            {
+                PixelPos.y -= velocity_y;
+            }
         }
     }
 
-    //check below grid
-    if(Rvelocity_y>0)
+    // down
+    if(velocity_y>0)
     {
-        if(pos_on_map.y+1 is wall)
+        // check below left
+        if(map->isWall(SDL_Point({MapPos.x-1,MapPos.y+1})))
         {
-            //wall = the wall below;
-            return wall;
+            SDL_Point wallPos;
+            wallPos.x = map->pixelPosTomapPos(SDL_Point({MapPos.x-1,MapPos.y+1})).x;
+            wallPos.y = map->pixelPosTomapPos(SDL_Point({MapPos.x-1,MapPos.y+1})).y;
+            SDL_Rect wall({wallPos.x,wallPos.y,gridWidth,gridHeight});
+            if(collisionBox(wall))
+            {
+                PixelPos.y -= velocity_y;
+            }
+        }
+        // chek below middle
+        if(map->isWall(SDL_Point({MapPos.x,MapPos.y+1})))
+        {
+            SDL_Point wallPos;
+            wallPos.x = map->pixelPosTomapPos(SDL_Point({MapPos.x,MapPos.y+1})).x;
+            wallPos.y = map->pixelPosTomapPos(SDL_Point({MapPos.x,MapPos.y+1})).y;
+            SDL_Rect wall({wallPos.x,wallPos.y,gridWidth,gridHeight});
+            if(collisionBox(wall))
+            {
+                PixelPos.y -= velocity_y;
+            }
+        }
+        // check below right
+        if(map->isWall(SDL_Point({MapPos.x+1,MapPos.y+1})))
+        {
+            SDL_Point wallPos;
+            wallPos.x = map->pixelPosTomapPos(SDL_Point({MapPos.x+1,MapPos.y+1})).x;
+            wallPos.y = map->pixelPosTomapPos(SDL_Point({MapPos.x+1,MapPos.y+1})).y;
+            SDL_Rect wall({wallPos.x,wallPos.y,gridWidth,gridHeight});
+            if(collisionBox(wall))
+            {
+                PixelPos.y -= velocity_y;
+            }
         }
     }
-    return wall;
 }
 
 void Runner::initclips()                // init render clips
 {
-    Current_Clip[0].x = 0000;
-    Current_Clip[0].y = 0000;
-    Current_Clip[0].w = 0000;
-    Current_Clip[0].h = 0000;
+    // Down
+    Clip[0].x = 0000;
+    Clip[0].y = 0000;
+    Clip[0].w = 0000;
+    Clip[0].h = 0000;
 
-    Current_Clip[1].x = 0000;
-    Current_Clip[1].y = 0000;
-    Current_Clip[1].w = 0000;
-    Current_Clip[1].h = 0000;
+    Clip[1].x = 0000;
+    Clip[1].y = 0000;
+    Clip[1].w = 0000;
+    Clip[1].h = 0000;
 
-    Current_Clip[2].x = 0000;
-    Current_Clip[2].y = 0000;
-    Current_Clip[2].w = 0000;
-    Current_Clip[2].h = 0000;
+    Clip[2].x = 0000;
+    Clip[2].y = 0000;
+    Clip[2].w = 0000;
+    Clip[2].h = 0000;
 
-    Current_Clip[3].x = 0000;
-    Current_Clip[3].y = 0000;
-    Current_Clip[3].w = 0000;
-    Current_Clip[3].h = 0000;
+    //Right
+    Clip[3].x = 0000;
+    Clip[3].y = 0000;
+    Clip[3].w = 0000;
+    Clip[3].h = 0000;
 
-    Current_Clip[4].x = 0000;
-    Current_Clip[4].y = 0000;
-    Current_Clip[4].w = 0000;
-    Current_Clip[4].h = 0000;
+    Clip[4].x = 0000;
+    Clip[4].y = 0000;
+    Clip[4].w = 0000;
+    Clip[4].h = 0000;
 
-    Current_Clip[5].x = 0000;
-    Current_Clip[5].y = 0000;
-    Current_Clip[5].w = 0000;
-    Current_Clip[5].h = 0000;
+    Clip[5].x = 0000;
+    Clip[5].y = 0000;
+    Clip[5].w = 0000;
+    Clip[5].h = 0000;
 
-    Current_Clip[6].x = 0000;
-    Current_Clip[6].y = 0000;
-    Current_Clip[6].w = 0000;
-    Current_Clip[6].h = 0000;
+    //Left
+    Clip[6].x = 0000;
+    Clip[6].y = 0000;
+    Clip[6].w = 0000;
+    Clip[6].h = 0000;
 
-    Current_Clip[7].x = 0000;
-    Current_Clip[7].y = 0000;
-    Current_Clip[7].w = 0000;
-    Current_Clip[7].h = 0000;
+    Clip[7].x = 0000;
+    Clip[7].y = 0000;
+    Clip[7].w = 0000;
+    Clip[7].h = 0000;
 
-    Current_Clip[8].x = 0000;
-    Current_Clip[8].y = 0000;
-    Current_Clip[8].w = 0000;
-    Current_Clip[8].h = 0000;
+    Clip[8].x = 0000;
+    Clip[8].y = 0000;
+    Clip[8].w = 0000;
+    Clip[8].h = 0000;
 
-    Current_Clip[9].x = 0000;
-    Current_Clip[9].y = 0000;
-    Current_Clip[9].w = 0000;
-    Current_Clip[9].h = 0000;
+    //Up
+    Clip[9].x = 0000;
+    Clip[9].y = 0000;
+    Clip[9].w = 0000;
+    Clip[9].h = 0000;
 
-    Current_Clip[10].x = 0000;
-    Current_Clip[10].y = 0000;
-    Current_Clip[10].w = 0000;
-    Current_Clip[10].h = 0000;
+    Clip[10].x = 0000;
+    Clip[10].y = 0000;
+    Clip[10].w = 0000;
+    Clip[10].h = 0000;
 
-    Current_Clip[11].x = 0000;
-    Current_Clip[11].y = 0000;
-    Current_Clip[11].w = 0000;
-    Current_Clip[11].h = 0000;
+    Clip[11].x = 0000;
+    Clip[11].y = 0000;
+    Clip[11].w = 0000;
+    Clip[11].h = 0000;
 }
--- src/Runner.h
+++ src/Runner.h
@@ -8,32 +8,42 @@ extern int mode;
 #include"Game.h"
 
 enum character_list{professor_hsieh,bear,soldier};
+enum face {Down,Left,Right,Up};
 class Runner:public BasicObject{
 private:
     std::string username;
+    face direction;             // character direction(used for render different graphs)
+    SDL_Rect* CurrentClip;      // use for render
     SDL_Point PixelPos;         // small grid on map
     SDL_Point MapPos;           // big grid on map
-    int Rvelocity_x;            // runner x velocity
-    int Rvelocity_y;            // runner y velocity
-    int Rvelocity;              // the velocity when pressing button
-    double Rstrength;           // the strength of runner
+    int velocity_x;             // runner x velocity
+    int velocity_y;             // runner y velocity
+    int velocity;               // the velocity when pressing button
+    double strength;            // the strength of runner
     int sprint_velocity;        // the velocity added when sprinting
+    int width;                  // rectangle width (for collision box)
+    int height;                 // rectangle height (for collision box)
+    int updateRate;             // different update rate according to running or walking
     Map* map;
-    Game &game = Game::GetGame();
+    Game &game;
 
 public:
-    Runner(character_list);
+    Runner(SDL_Point&, SDL_Point&,character_list=bear);
     ~Runner();
     bool handleEvents(SDL_Event &e);
     void update();
     void move();
-    bool collision(Wall*);
-    Wall* getwall()const;
+    bool collisionBox(SDL_Rect&);
+    void checkCollision();
     void initclips();
     void setMapPos(SDL_Point&);         // set big grid on map
-    void setPixelPos(SDL_Point);        // set small grid on map (center)
-    SDL_Point getMapPos() const;       // get big grid on map
-    SDL_Point getPixelPos()const;      // get small grid on map (center)
+    void setPixelPos(SDL_Point&);       // set small grid on map (center)
+    void updateMapPos(SDL_Point&);      // ask map for big grid and update MapPos
+    SDL_Point getMapPos() const;        // get big grid on map
+    SDL_Point getPixelPos()const;       // get small grid on map (center)
+
+    static const int gridWidth; // the width of grid on map
+    static const int gridHeight;// the height of grid on map
 };
 
 
