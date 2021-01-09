cd22f39c529136ca59d17964cc320d6ce68fed1e
lsy1205 <75497265+lsy1205@users.noreply.github.com>
2021-01-09 17:18:25 +0800

    includeallheader.h

---
+++ src/IncludeAllHeader.h
@@ -0,0 +1,26 @@
+//
+// Created by 劉瑄穎 on 2021/1/9.
+//
+
+#ifndef INCLUDEALLHEADER_H
+#include<cmath>
+#include<string>
+#include<vector>
+#include<queue>
+#include<iostream>
+#include"includeSDL.h"
+#include"BasicObject.h"
+#include"Button.h"
+#include"Files.h"
+#include"Game.h"
+#include"Hunter.h"
+#include"Item.h"
+#include"Layer.h"
+#include"Map.h"
+#include"Mission.h"
+#include"Runner.h"
+#include"UserEvent.h"
+#include"Wall.h"
+#define INCLUDEALLHEADER_H
+
+#endif //SRC_INCLUDEALLHEADER_H
